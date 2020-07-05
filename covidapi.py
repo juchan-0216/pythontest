5c6b1d724a2165fff4a747e6372cdea504de4c59
주in <60591895+juchan-0216@users.noreply.github.com>
2020-07-05 18:52:35 +0900

    covidapi version1
    created covid api

---
+++ covidapi.py
@@ -0,0 +1,21 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+
+def covkr():
+    html = requests.get('https://coronaboard.kr')
+    html = html.content.decode('utf8')
+    jsonString = html.split('jsonData = ')[1]
+    jsonString = jsonString.split(';')[0]
+    dictionary = json.loads(jsonString)['chartForGlobal']['KR']
+
+    result = {
+        "confirmed": dictionary["confirmed_acc"][-1],
+        "comfirmed_today": dictionary["confirmed"][-1],
+        "death": dictionary["death_acc"][-1],
+        "death_today": dictionary["death"][-1],
+        "cured": dictionary["released_acc"][-1],
+        "cured_today": dictionary["released"][-1]
+    }
+    return result
+print(covkr())
